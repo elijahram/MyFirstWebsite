f76524aac6f6b882bfeb60a977240ebb6aebec11
eLoyal <elijahbramos@gmail.com>
2022-12-09 23:44:29 -0600

    created click alert for new 'REDBOX' id

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log("This is me learning HTML, CSS, and JAVASCRIPT");
+document.getElementById('REDBOX').addEventListener('click', function(){
+    alert('new function');
+});
 document.getElementById('box').addEventListener('click', function(){
     alert('I got clicked');
 });
